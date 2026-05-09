682bb80b410c7d380ed7bb0af494ca9598f275fa
molim-team <YOUR_MOLIM_EMAIL>
2026-05-09 12:26:12 +0300

    fix cards

--- static/js/main.js
+++ static/js/main.js
@@ -97,6 +97,7 @@ if (grid) {
         <a class="btn-details" style="background:white; color:#ff4500; border:none; cursor:pointer;" onclick="shareScholarship('${s.id}', '${s.name}', '${s.country}')">📤 شارك المنحة</a>
       `;
       grid.appendChild(card);
+      observer.observe(card);
     });
   })
   .catch(err => console.error('خطأ:', err));
